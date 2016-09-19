da99bc9d98c5884a3855bc89bd2002ea33d14eb1
bc.0x4861 <bc.0x4861@gmail.com>
2016-09-20 02:27:59 +0900

    [Add] Added protocol command interface & modified play button

---
+++ app/src/main/java/app/park/com/bluetooth/Protocol.java
@@ -0,0 +1,27 @@
+package app.park.com.bluetooth;
+
+/**
+ * Defines Protocol message.
+ */
+public interface Protocol {
+    // msg protocol
+    // Case command is play -> play////playnumber[1,2,3]
+    // Case command is control -> contorl////speed////handle[0,1]////signalLight[0,1,2]////accel[0,1]////break[0,1]
+    // Case old version command -> cmd////velocity////score
+
+    public static final String SEPARATOR  = "////";
+    public static final String CMD_PLAY  = "play";
+    public static final String CMD_PAUSE = "pause";
+    public static final String CMD_STOP  = "stop";
+    public static final String CMD_CTRL  = "control";
+
+    public static final int  INDEX_CMD = 0;
+    public static final int  INDEX_SPEED = 1;
+    public static final int  INDEX_HANDLE = 2;
+    public static final int  INDEX_SIGNALLIGHT = 3;
+    public static final int  INDEX_ACCEL = 4;
+    public static final int  INDEX_BREAK = 5;
+
+    public static final int  INDEX_PLAY_NUM = 1; // If command is play, next value is play video number
+    public static final int  INDEX_SCORE = 2;   // Should be removed.
+}
