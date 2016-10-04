ca2e1642a4e9a2e11ea5db883ec0b2f7ab27c32a
bc.0x4861 <bc.0x4861@gmail.com>
2016-10-04 23:23:18 +0900

    [Add] Mission1,2,3 Support

---
+++ app/src/main/java/app/park/com/task/MissionManager.java
@@ -0,0 +1,60 @@
+package app.park.com.task;
+
+public class MissionManager {
+    public static final String TAG = MissionManager.class.getSimpleName();
+
+    public static String currentVideo;
+    public static MissionOne mOne;
+    public static MissionTwo mTwo;
+    public static MissionThree mThree;
+
+    public static void setVideoNumber(String number) {
+        currentVideo = number;
+    }
+
+    public static int getVideoNumber() {
+        int number;
+        if (currentVideo == null) {
+            number = 1; // defalt video
+        } else {
+            number = Integer.parseInt(currentVideo);
+        }
+        return number;
+    }
+
+    public static int validate(String[] arr, long currentTime) {
+        int result = 0;
+        int videoNum = getVideoNumber();
+        switch (videoNum) {
+            case 1:
+                result = mOne.validate(arr, currentTime);
+                break;
+            case 2:
+                result = mTwo.validate(arr, currentTime);
+                break;
+            case 3:
+                result = mThree.validate(arr, currentTime);
+                break;
+            default:
+                break;
+        }
+        return  result;
+    }
+
+    public static void reInit() {
+        int videoNum = getVideoNumber();
+        switch (videoNum) {
+            case 1:
+                mOne.reInit();
+                break;
+            case 2:
+                mTwo.reInit();
+                break;
+            case 3:
+                mThree.reInit();
+                break;
+            default:
+                break;
+        }
+    }
+}
